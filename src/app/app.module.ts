0ac83002d8808a3bbc3dd0965e6f4aff20a3f3e9
Simon BERNARD <sbernard.prestataire2@voyages-sncf.com>
2017-09-06 09:26:50 +0200

    (feat) Stabilized app module & component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { EffectsModule } from '@ngrx/effects';
 import { RouterModule } from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdToolbarModule, MdIconModule } from '@angular/material';
 
 import 'rxjs/add/operator/do';
@@ -13,12 +13,18 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/race';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/switchMapTo';
+import 'rxjs/add/operator/distinct';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-
 import { appRoutes } from './app.routing';
 import { appReducer } from './shared/app.reducer';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
@@ -28,10 +34,12 @@ import { appReducer } from './shared/app.reducer';
     MdIconModule,
     MdButtonModule,
     MdToolbarModule,
+    BrowserAnimationsModule,
+    AngularFireDatabaseModule,
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({}),
     StoreModule.forRoot({app: appReducer}),
-    AngularFireModule.initializeApp(environment.firebase),
+    StoreDevtoolsModule.instrument({}),
+    AngularFireModule.initializeApp(environment.firebase, 'expenses'),
     RouterModule.forRoot(
       appRoutes, { enableTracing: false }
     ),
